c7daa62c5c10a387c8b76a00c9080bb49dc3aec6
Chaeeun Kim <kimemily1227@gmail.com>
2019-10-10 12:03:36 -0700

    Added animal files

---
+++ animal.cpp
@@ -0,0 +1,10 @@
+//
+// Created by kimem on 2019-10-10.
+//
+
+#include "animal.hpp"
+
+ostream &operator<<(ostream &os, const animal &animal) {
+    os << "overloaded << operator"<< endl;
+    return os;
+}
